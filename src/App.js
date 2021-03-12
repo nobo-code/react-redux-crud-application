a80ce68cfc24475163554bbfe0807412664df671
前原昴 <maeharanoboru@MBP.local>
2021-03-12 17:30:01 +0900

    create class component and functional componrnt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import react from 'react';
 
-function App() {
+// function App() {
+//   return (
+//    <>
+//     <label htmlFor="bar">bar</label>
+//     <input type="text" onChange={() => {console.log("I am clicked!")}} />;
+//    </>
+//   )
+// }
+
+const App = () => {
   return (
-   <>
-    <label htmlFor="bar">bar</label>
-    <input type="text" onChange={() => {console.log("I am clicked!")}} />;
-   </>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
